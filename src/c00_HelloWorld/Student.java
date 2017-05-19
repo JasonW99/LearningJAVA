9793d61daf4d1daa918f02096c82934316b4600e
Jasonw <ningweiw@gmail.com>
2017-05-19 00:34:40 -0700

    hello world

---
+++ src/c00_HelloWorld/Student.java
@@ -0,0 +1,38 @@
+package c00_HelloWorld;
+
+public class Student {
+    public int age = 23;
+    //(member) field: the field value is stick to the object. and we set a initial value to it as 23.
+    //i.e. some objects equal to 18, some objects equal to 22.
+    private final String name;
+    //(constant) field: once assigned value to the field, the value cannot change any more.
+    private static String school;
+    //(static) field: the field value is stick to the class.
+    //i.e. once 'Student.school' is assigned by 'UCSD', then all objects under 'Student' will be from 'USCD'.
+    //On the other hand, if the 'Student.school' is changed to 'UCLA', then all objects under 'Student' will be from 'USLA'.
+    private static final String GREETINGS = "Hello";
+    //(static constant) field: the field value is stick to the class, once assigned value, the value can change no more.
+
+    //define the constructor
+    public Student(String name) {
+        this.name = name;
+    }
+
+    //define some method
+    public int getAge() {    //(member) method, getter or accessor
+        return age;
+    }
+
+    public void setAge(int age) {   //(member) method, setter
+        this.age = age;
+    }
+
+    public String getName(){    //(member) method, getter or accessor
+        return name;
+    }
+
+    public void sayHello(){    //(member) method
+        int a = 5;  // local variable
+        System.out.println(GREETINGS + "! I am " + name + ". I am " + age);
+    }
+}
---
+++ src/c00_HelloWorld/hello2.java
@@ -0,0 +1,18 @@
+package c00_HelloWorld;
+
+public class hello2 {
+    public static void main(String[] args){
+        Student firstStudent = new Student("Tom");
+        Student secondStudent = new Student("Jerry");
+        firstStudent.setAge(18);
+        //secondStudent.setAge(10);
+        firstStudent.sayHello();
+        secondStudent.sayHello();
+        Student thirdStudent = null;
+        try{
+            thirdStudent.sayHello();
+        }catch(NullPointerException e){
+            System.out.println("Expected error occured! -" + e.toString());
+        }
+    }
+}
