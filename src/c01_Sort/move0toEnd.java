7574408871c428a3b8ba57f4914ba25221514dcf
Jasonw <ningweiw@gmail.com>
2017-05-20 22:05:24 -0700

    sort method 3

--- src/c01_Sort/move0toEnd.java
+++ src/c01_Sort/move0toEnd.java
@@ -7,6 +7,7 @@ public class move0toEnd {
         if(array == null || array.length <=1) {
             return array;
         }
+        // integers between start0 and end0 are all zeros
         int start0 = 0;
         for(int end0 = 0; end0 < array.length; end0++){
             if(array[end0] != 0){
@@ -30,3 +31,30 @@ public class move0toEnd {
         System.out.println(Arrays.toString(input));
     }
 }
+
+/**
+public class move0toEnd {
+    public int[] solve(int[] array) {
+        if (array == null || array.length <= 1) {
+            return array;
+        }
+        int start0 = 0;
+        for (int end0 = 0; end0 < array.length; end0++) {
+            if (array[end0] != 0) {
+                array[start0++] = array[end0];
+            }
+        }
+        while (start0 < array.length) {
+            array[start0++] = 0;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        move0toEnd test = new move0toEnd();
+        int[] input = {1, 1, 0, 0, 3, 2};
+        input = test.solve(input);
+        System.out.println(Arrays.toString(input));
+    }
+}
+**/
