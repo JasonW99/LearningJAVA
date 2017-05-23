7806d35a47b8466ec865ba0a9292bfe7b8c6b23f
Jasonw <ningweiw@gmail.com>
2017-05-22 22:37:14 -0700

    minor updates

--- src/c01_Sort/move0toEnd.java
+++ src/c01_Sort/move0toEnd.java
@@ -4,15 +4,16 @@ import java.util.Arrays;
 
 public class move0toEnd {
     public int[] solve(int[] array) {
-        if(array == null || array.length <=1) {
+        if (array == null || array.length <= 1) {
             return array;
         }
-        // integers between start0 and end0 are all zeros
-        int start0 = 0;
-        for(int end0 = 0; end0 < array.length; end0++){
-            if(array[end0] != 0){
-                swap(array, end0, start0);
-                start0++;
+        int start = 0;
+        int end = 0;
+        while (end < array.length) {
+            if (array[end] == 0) {
+                end++;
+            } else {
+                swap(array, end++, start++);
             }
         }
         return array;
@@ -31,30 +32,3 @@ public class move0toEnd {
         System.out.println(Arrays.toString(input));
     }
 }
-
-/**
-public class move0toEnd {
-    public int[] solve(int[] array) {
-        if (array == null || array.length <= 1) {
-            return array;
-        }
-        int start0 = 0;
-        for (int end0 = 0; end0 < array.length; end0++) {
-            if (array[end0] != 0) {
-                array[start0++] = array[end0];
-            }
-        }
-        while (start0 < array.length) {
-            array[start0++] = 0;
-        }
-        return array;
-    }
-
-    public static void main(String[] args) {
-        move0toEnd test = new move0toEnd();
-        int[] input = {1, 1, 0, 0, 3, 2};
-        input = test.solve(input);
-        System.out.println(Arrays.toString(input));
-    }
-}
-**/
--- src/c01_Sort/quickSort.java
+++ src/c01_Sort/quickSort.java
@@ -3,16 +3,16 @@ package c01_Sort;
 import java.util.Arrays;
 
 public class quickSort {
-    public int[] solve(int[] array){
-        if(array == null || array.length <= 1){
+    public int[] solve(int[] array) {
+        if (array == null || array.length <= 1) {
             return array;
         }
         solve(array, 0, array.length - 1);
         return array;
     }
 
-    private void solve(int[] array, int start, int end){
-        if(start >= end){
+    private void solve(int[] array, int start, int end) {
+        if (start >= end) {
             return;
         }
         int p = pivital(array, start, end);
@@ -20,13 +20,13 @@ public class quickSort {
         solve(array, p + 1, end);
     }
 
-    private int pivital(int[] array, int start, int end){
+    private int pivital(int[] array, int start, int end) {
         int left = start;
         int right = end - 1;
-        while(left <= right){
-            if(array[left] <= array[end]){
+        while (left <= right) {
+            if (array[left] <= array[end]) {
                 left++;
-            }else{
+            } else {
                 swap(array, left, right);
                 right--;
             }
@@ -35,15 +35,15 @@ public class quickSort {
         return left;
     }
 
-    private void swap(int[] array, int i, int j){
+    private void swap(int[] array, int i, int j) {
         int temp = array[j];
         array[j] = array[i];
         array[i] = temp;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         quickSort test = new quickSort();
-        int[] input = {2, -2, 31, 0, 1};
+        int[] input = {4, 2, 1, 6, 3, 5};
         input = test.solve(input);
         System.out.println(Arrays.toString(input));
     }
