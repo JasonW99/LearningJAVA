9327a9645b69a05d5e8821b421c09fa98959ccc2
JasonW <ningweiw@gmail.com>
2017-05-23 22:48:19 -0700

    linked list update 1

--- src/c04_QueueStack/queueBY2stack.java
+++ src/c04_QueueStack/queueBY2stack.java
@@ -8,27 +8,42 @@ public class queueBY2stack {
     private Deque<Integer> B;
 
     public queueBY2stack() {
-        A = new LinkedList<Integer>();
-        B = new LinkedList<Integer>();
+        A = new LinkedList<>();
+        B = new LinkedList<>();
     }
 
     public Integer poll() {
-        return null;
+        if (this.isEmpty()) { //note that if A is empty, then it cannot 'pop', but can 'peek'.
+            return null;
+        }
+        if (B.isEmpty()) {
+            move2B();
+        }
+        return B.pop();
     }
 
     public void offer(int element) {
-
+        A.push(element);
     }
 
     public Integer peek() {
-        return null;
+        if (B.isEmpty()) {
+            move2B();
+        }
+        return B.peek();
     }
 
     public int size() {
-        return 0;
+        return A.size() + B.size();
     }
 
     public boolean isEmpty() {
-        return false;
+        return A.isEmpty() && B.isEmpty();
+    }
+
+    private void move2B() {
+        while (!A.isEmpty()) {
+            B.push(A.pop());
+        }
     }
 }
