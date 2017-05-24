ad92caf53415140a36523c9ce06c98679c5670e6
Jasonw <ningweiw@gmail.com>
2017-05-23 21:31:09 -0700

    Linked List

---
+++ src/c04_QueueStack/queueBY2stack.java
@@ -0,0 +1,34 @@
+package c04_QueueStack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class queueBY2stack {
+    private Deque<Integer> A;
+    private Deque<Integer> B;
+
+    public queueBY2stack() {
+        A = new LinkedList<Integer>();
+        B = new LinkedList<Integer>();
+    }
+
+    public Integer poll() {
+        return null;
+    }
+
+    public void offer(int element) {
+
+    }
+
+    public Integer peek() {
+        return null;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+}
