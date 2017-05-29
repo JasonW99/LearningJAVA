49c3ec5364b6bd0c75ed8be16bcf11f2cdabc8f5
Jasonw <ningweiw@gmail.com>
2017-05-28 17:26:46 -0700

    TreeNode update2

---
+++ src/c05_BinartTree/IterativePostorderTraversal.java
@@ -0,0 +1,35 @@
+package c05_BinartTree;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class IterativePostorderTraversal {
+    public List<Integer> solve(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<Integer>();
+        }
+        List<Integer> result = new ArrayList<Integer>();
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+        TreeNode latest = null; // the latest number printed in the traversal list.
+        while (!stack.isEmpty()) {
+            root = stack.peek();
+            if (root.right == null || root.right == latest) {
+                latest = stack.pop();
+                result.add(latest.key);
+            } else {
+                root = root.right;
+                while (root != null) {
+                    stack.push(root);
+                    root = root.left;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/c05_BinartTree/IterativePreorderTraversal.java
@@ -0,0 +1,31 @@
+package c05_BinartTree;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class IterativePreorderTraversal {
+    public List<Integer> solve(TreeNode root) {
+        if (root == null) {
+            return new LinkedList<Integer>();
+        }
+        List<Integer> result = new ArrayList<Integer>();
+        Deque<TreeNode> stack = new LinkedList<TreeNode>();
+        while (root != null) {
+            result.add(root.key);
+            stack.push(root);
+            root = root.left;
+        }
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            root = root.right;
+            while (root != null) {
+                result.add(root.key);
+                stack.push(root);
+                root = root.left;
+            }
+        }
+        return result;
+    }
+}
