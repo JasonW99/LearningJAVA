69e7301d8284d6343d0c51f985524408ce5c16d6
Jasonw <ningweiw@gmail.com>
2017-06-17 01:11:20 -0700

    string update 9

--- src/c09_StringII/Shuffle.java
+++ src/c09_StringII/Shuffle.java
@@ -7,34 +7,36 @@ public class Shuffle {
         if (array == null || array.length <= 3) {
             return array;
         }
-        shuffleRecur(array, 0, array.length / 2 * 2 - 1);
+        shuffleRecur(array, 0, array.length / 2 * 2 - 1, array.length / 2);
         return array;
     }
 
-    private void shuffleRecur(int[] array, int start, int end) {
-        if(end - start < 2) {
+    private void shuffleRecur(int[] array, int start, int end, int level) {
+        if (level == 0) {
             return;
         }
-        int mid = start + (end - start) / 2;
-        int lm = start + (end - start) / 4;
-        int rm = end - (end - start) / 4;
-        reverse(array, lm, rm);
-        shuffleRecur(array, start, mid);
-        shuffleRecur(array, mid + 1, end);
+        shift(array, start + 1, end - 1);
+        shuffleRecur(array, start + 1, end - 1, level - 1);
+    }
+
+    private void shift(int[] array, int start, int end) {
+        int k = (end - start + 1) / 2;
+        reverse(array, start, start + k - 1);
+        reverse(array, start + k, end);
+        reverse(array, start, end);
     }
 
     private void reverse(int[] array, int start, int end) {
-        int start2 = start + (end - start) / 2 + 1;
-        while (start2 <= end) {
+        while (start < end) {
             int temp = array[start];
-            array[start++] = array[start2];
-            array[start2++] = temp;
+            array[start++] = array[end];
+            array[end--] = temp;
         }
     }
 
     public static void main(String[] args) {
         Shuffle test = new Shuffle();
-        int[] input = new int[]{1, 2, 3, 4, 5, 6};
+        int[] input = new int[]{1, 2, 3, 4, 5, 6, 7};
         System.out.println(Arrays.toString(test.solve(input)));
     }
 }
