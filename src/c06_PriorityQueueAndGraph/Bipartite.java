f548b16e2e646e67c4b0b9bad4e437a8bac2cf5a
Jasonw <ningweiw@gmail.com>
2017-06-22 23:43:35 -0700

    small modify

--- src/c06_PriorityQueueAndGraph/Bipartite.java
+++ src/c06_PriorityQueueAndGraph/Bipartite.java
@@ -7,18 +7,22 @@ import java.util.Map;
 public class Bipartite {
     public boolean solve(List<GraphNode> graph) {
         Map<GraphNode, Integer> map = new HashMap<GraphNode, Integer>();
+        int group = 0;
+        Integer currGroup = null;
+        Integer neighborGroup = null;
         for (GraphNode curr : graph) {
-            Integer color = map.get(curr);
-            if (color == null) {
-                color = 1;
-                map.put(curr, color);
+            currGroup = map.get(curr);
+            if (currGroup == null) {
+                currGroup = 1;
+                map.put(curr, currGroup);
             }
-            for (GraphNode currNeighbor : curr.neighbors) {
-                Integer neighborColor = map.get(currNeighbor);
-                if (neighborColor == null){
-                    map.put(currNeighbor, 1 - color);
-                } else if (neighborColor == color) {
+            for (GraphNode neighbor : curr.neighbors) {
+                neighborGroup = map.get(neighbor);
+                if (neighborGroup == currGroup) {
                     return false;
+                }
+                if (neighborGroup == null) {
+                    map.put(neighbor, 1 - currGroup);
                 }
             }
         }
