871fb90d517345470d1a3fe1985e16cb2b62a39d
JasonW99 <ningweiw@gmail.com>
2017-08-15 23:24:38 -0700

    review

--- src/c12_DynamicProgramming01/ArrayHoperI.java
+++ src/c12_DynamicProgramming01/ArrayHoperI.java
@@ -1,5 +1,6 @@
 package c12_DynamicProgramming01;
 
+// O(n^2) solution
 public class ArrayHoperI {
     public boolean solve(int[] array) {
         boolean[] result = new boolean[array.length];
@@ -15,3 +16,22 @@ public class ArrayHoperI {
         return result[array.length - 1];
     }
 }
+
+/**
+ * O(nk) solution
+public class Solution {
+    public boolean canJump(int[] array) {
+        boolean[] result = new boolean[array.length];
+        result[result.length - 1] = true;
+        for (int i = result.length - 2; i >= 0; i--) {
+            for (int j = 1; j <= array[i]; j++) {
+                if (result[i + j]) {
+                    result[i] = true;
+                    break;
+                }
+            }
+        }
+        return result[0];
+    }
+}
+**/
