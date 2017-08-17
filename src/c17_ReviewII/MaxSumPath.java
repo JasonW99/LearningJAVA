8b49e261b58e01d32c3aa5e2aea5e8443b48e55e
JasonW99 <ningweiw@gmail.com>
2017-08-17 03:24:13 -0700

    dp review

--- src/c17_ReviewII/MaxSumPath.java
+++ src/c17_ReviewII/MaxSumPath.java
@@ -1,4 +1,26 @@
 package c17_ReviewII;
 
+import c05_BinartTree.TreeNode;
+
 public class MaxSumPath {
+    public int solve(TreeNode root) {
+        int[] max = new int[1];
+        max[0] = Integer.MIN_VALUE;
+        findMax(root, max);
+        return max[0];
+    }
+
+    private int findMax(TreeNode root, int[] max) {
+        if (root == null) {
+            return 0;
+        }
+        int left = findMax(root.left, max);
+        int right = findMax(root.right, max);
+        if (root.left == null || root.right == null) {
+            return root.key + right + left;
+        }
+        max[0] = Math.max(max[0], root.key + left + right);
+        return root.key + Math.max(left, right);
+    }
+
 }
---
+++ src/c17_ReviewII/MinCutPalindromes.java
@@ -0,0 +1,15 @@
+package c17_ReviewII;
+
+public class MinCutPalindromes {
+    public int solve(String input) {
+        if (input == null || input.length() <= 1) {
+            return 0;
+        }
+
+
+
+
+
+
+    }
+}
